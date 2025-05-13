bb0f6b39e9011a0a12135670bc37acd917bdb2b4
HanlinMiao <hanlin980505@gmail.com>
2025-05-12 20:59:59 -0400

    Customize

--- components/intro.tsx
+++ components/intro.tsx
@@ -48,12 +48,9 @@ export default function Intro() {
             initial= {{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
         >
-            <span className="font-bold">Hello, I'm Hanlin.</span> I'm a{" "}
-            <span className="font-bold">backend-focused full-stack</span> software engineer with{" "}
-            <span className="font-bold">3 years</span> of experience. I enjoy
-            building <span className="italic">sites & apps</span>. My focus is{" "}
-            <span className="underline">Python/Java</span> and {" "}
-            <span className="underline">React (Next.js)</span>.
+            Hello, I'm Hanlin. I'm a{" "}
+            <span className="font-bold underline">U.S. citizen</span> and experienced Software Engineer with a strong foundation in <span className="font-bold underline">Backend Development, DevOps, and scalable system design</span>.
+            I specialize in <span className="font-bold underline">Python and Django</span>, with a proven track record of optimizing performance, reducing infrastructure costs, and enhancing developer productivity.
         </motion.h1>
         <motion.div
             className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
